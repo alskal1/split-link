b108874687cd7be5b9c6ad39a48ac0f8832e1261
ahyeon <uiyn@naver.com>
2026-09-03 14:18:17 +0900

    feat: 버튼 컴포넌트에 비활성화 여부 설정 추가

--- FE/src/components/Button.tsx
+++ FE/src/components/Button.tsx
@@ -4,6 +4,7 @@ interface ButtonProps {
   bgColor?: string;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
 /**
@@ -13,20 +14,25 @@ interface ButtonProps {
  * @param bgcolor 버튼 배경 색상
  * @param className 클래스 속성
  * @param onClick 클릭 이벤트
+ * @param disabled 비활성화 여부
  * @returns
  */
 export default function Button({
   title,
-  textColor = "#000",
+  textColor,
   bgColor,
   className = "",
   onClick,
+  disabled = false,
 }: ButtonProps) {
   return (
     <button
-      className={`${className} cursor-pointer font-bold px-5 py-2`}
+      className={`${className} inline-flex items-center justify-center font-bold px-5 py-2 ${
+        disabled ? "cursor-not-allowed opacity-50" : "cursor-pointer"
+      }`}
       style={{ color: textColor, backgroundColor: bgColor }}
       onClick={onClick}
+      disabled={disabled}
     >
       {title}
     </button>
